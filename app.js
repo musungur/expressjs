ab0b9a438bd0c5763f40144d17c49206f7ee13aa
robertm <roberttttm@gmail.com>
2021-07-22 17:36:12 +0300

    nodejsExpress first-commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const express = require("express")
+
+const app = express()
+
+
+function server(){
+	const port = 3000
+	const host = "127.0.0.1"
+
+	console.log("Server listening on port "+port+".")
+	console.log("Host IP: "+host+".")
+
+}
+
+function payload(){
+	var nm = "Robert Musungu Abisai"
+	var msg = "this is My First js App Test ", name ="My Name is "+nm+"."
+	console.log("This is payload function")
+
+
+	console.log(msg, name)
+}
+
+// console.log(server(),payload())
